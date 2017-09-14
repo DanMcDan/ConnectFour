58f24bba9cadaf16c5227b0f9e053060d547349c
DanMcDan <Daniel.McCann@edu.sait.ca>
2017-09-13 18:16:06 -0600

    Created more gui elements

--- src/gui/PlayerFrame.java
+++ src/gui/PlayerFrame.java
@@ -8,38 +8,33 @@ import javax.swing.*;
 public class PlayerFrame extends JFrame{
 	private static final long serialVersionUID = -5683163455384492681L;
 	
-	private JPanel menuPanel;
-	private JPanel gamePanel;
+	private JPanel wrapper = new JPanel();
+	private JPanel menuPanel = new MenuPanel();
+	private JPanel gamePanel = new GamePanel();
 	
 	
 	
 	public PlayerFrame(String s) {
 		super(s);
 		setLayout(new BorderLayout());
-		add(new JPanel(), BorderLayout.CENTER);
-		
+		add(wrapper, BorderLayout.CENTER);
+
 		//Listener defines what happens when the application is closed
 		addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent e) {
 				//add closing operations as needed, in this area
-				
-				
-				
 				System.exit(0);
 			}
 		});
 		
 		
 		
-		setBounds(0, 0, 700, 400);
+		setBounds(200, 200, 700, 400);
 		setVisible(true);
 		setResizable(false);
-		
-		generateMenuPanel();
-	}
-
-	private void generateMenuPanel() {
-		
+		wrapper.setLayout(new BorderLayout());
+		wrapper.add(menuPanel, BorderLayout.EAST);
+		wrapper.add(gamePanel, BorderLayout.CENTER);
 	}
 }
