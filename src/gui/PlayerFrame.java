0f37a280aded9238be070528cd988bf4e174478c
DanMcDan <Daniel.McCann@edu.sait.ca>
2017-09-13 22:29:53 -0600

    Progress made on the layout, as well as chat being implimented

--- src/gui/PlayerFrame.java
+++ src/gui/PlayerFrame.java
@@ -1,40 +1,64 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import javax.swing.*;
 
+/**
+ * Frame object intended to be part of the client side of the Connect Four game.<br>
+ * The user would play the actual game in a PlayerFrame and be sending turns to the server.
+ * @author Daniel McCann
+ *
+ */
 public class PlayerFrame extends JFrame{
 	private static final long serialVersionUID = -5683163455384492681L;
 	
-	private JPanel wrapper = new JPanel();
-	private JPanel menuPanel = new MenuPanel();
-	private JPanel gamePanel = new GamePanel();
+	private JPanel wrapper 		= new JPanel(new GridBagLayout());
+	private JPanel menuPanel 	= new MenuPanel();
+	private JPanel gamePanel 	= new GamePanel();
 	
 	
-	
+	/**
+	 * Constructor for the window to be used as the game area for Connect Four.<br>
+	 * Sets bounds for window and constructs the <i>MenuPanel</i> and <i>GamePanel</i>
+	 * @param s String value to set the title of the window.
+	 */
 	public PlayerFrame(String s) {
 		super(s);
 		setLayout(new BorderLayout());
 		add(wrapper, BorderLayout.CENTER);
+		
+		
+		//Doing things like setting the window size and making it visible
+		setBounds(200, 200, 700, 400);
+		setVisible(true);
+		setResizable(false);
 
 		//Listener defines what happens when the application is closed
 		addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent e) {
-				//add closing operations as needed, in this area
+				//TODO: add closing operations as needed, in this area
 				System.exit(0);
 			}
 		});
 		
 		
+		//set GridBagConstraints for the wrapper panel
 		
-		setBounds(200, 200, 700, 400);
-		setVisible(true);
-		setResizable(false);
-		wrapper.setLayout(new BorderLayout());
-		wrapper.add(menuPanel, BorderLayout.EAST);
-		wrapper.add(gamePanel, BorderLayout.CENTER);
+		GridBagConstraints c = new GridBagConstraints();
+		c.gridx = 0;
+		c.gridy = 0;
+		c.weightx = 0.7;
+		c.weighty = 1;
+		c.fill = GridBagConstraints.BOTH;
+		//Begin adding to wrapper
+		wrapper.add(gamePanel, c);
+		c.gridx++;
+		c.weightx = 0.3;
+		wrapper.add(menuPanel, c);	//adding the game UI and the menu to the wrapper panel
 	}
 }
